bbd5d1a35ae2d23176d9818dbedf94cd97a26cd3
healey-o <150302176+healey-o@users.noreply.github.com>
2024-10-30 19:16:17 +1100

    Added login and signup screens

--- app.py
+++ app.py
@@ -2,8 +2,27 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#Welcome page when not signed in
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+#Login/signup pages
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+#Main page after login
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 app.run(debug=True, reloader_type='stat', port=5000)
